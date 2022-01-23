4c4ac43b4665ed8df73b582716584a8f339a9725
cs <siwon.cezary@gmail.com>
2022-01-23 11:27:04 +0100

    day 3 - admin functions

--- src/main/java/pl/sda/springwebmvc/controller/BookController.java
+++ src/main/java/pl/sda/springwebmvc/controller/BookController.java
@@ -4,12 +4,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.springwebmvc.model.Book;
 import pl.sda.springwebmvc.model.BookRepository;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/book")
@@ -51,5 +54,47 @@ public class BookController {
         return "/book/list";
     }
 
+    @GetMapping("/admin/list")
+    public String adminList(Model model){
+        final List<Book> bookList = books.findAll();
+        model.addAttribute("books", bookList);
+        return "/book/admin-list";
+    }
 
+    @GetMapping("/admin/delete/{isbn}")
+    public String deleteBook(@PathVariable String isbn, Model model){
+        if (books.delete(isbn)){
+            model.addAttribute("isbn", isbn);
+            return "/book/admin-confirm-delete";
+        } else {
+            model.addAttribute("message", "Brak takiej książki! Usunięcie niemożliwe!");
+            return "error";
+        }
+    }
+
+    @GetMapping("/admin/update/{isbn}")
+    public String updateBookForm(@PathVariable String isbn, Model model){
+        final Optional<Book> optionalBook = books.findById(isbn);
+        if (optionalBook.isPresent()){
+            model.addAttribute("book", optionalBook.get());
+            return "/book/admin-update-form";
+        } else {
+            model.addAttribute("message", "Nie można edytować, brak książki o podanym ISBN!");
+            return "error";
+        }
+    }
+
+    @PostMapping("/admin/update")
+    public String updateBook(@Valid @ModelAttribute Book book, Errors errors, Model model){
+        if(errors.hasErrors()){
+            return "/book/admin-update-form";
+        } else{
+            if (books.update(book)){
+                return "redirect:/book/admin/list";
+            } else {
+                model.addAttribute("message", "Próba edycji nieistniejącej książki!");
+                return "error";
+            }
+        }
+    }
 }
--- src/main/java/pl/sda/springwebmvc/model/BookRepository.java
+++ src/main/java/pl/sda/springwebmvc/model/BookRepository.java
@@ -3,10 +3,7 @@ package pl.sda.springwebmvc.model;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 public class BookRepository {
@@ -17,7 +14,7 @@ public class BookRepository {
         final Book book1 = Book.builder()
                 .authors("Adam Nowak")
                 .title("Java")
-                .isbn("11")
+                .isbn("978-83-246-7762-7")
                 .price(new BigDecimal("20.5"))
                 .firstEdition(false)
                 .publishingYear(2000)
@@ -27,7 +24,7 @@ public class BookRepository {
         save(Book.builder()
                 .price(new BigDecimal("11.80"))
                 .publishingYear(2020)
-                .isbn("24")
+                .isbn("978-83-246-2084-5")
                 .authors("Bloch")
                 .title("Effective Java")
                 .publisher("Pearson")
@@ -39,7 +36,36 @@ public class BookRepository {
         books.put(book.getIsbn(), book);
     }
 
+    public boolean delete(String isbn){
+//        if (books.remove(isbn)!=null) {
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+        return books.remove(isbn) != null ? true : false;
+    }
+
     public List<Book> findAll() {
         return new ArrayList<>(books.values());
+    }
+
+    public Optional<Book> findById(String isbn){
+        return Optional.ofNullable(books.get(isbn));
+    }
+
+    public boolean update(Book book){
+        final Optional<Book> bookOptional = findById(book.getIsbn());
+        if (bookOptional.isPresent()){
+            Book original = bookOptional.get();
+            original.setTitle(book.getTitle());
+            original.setAuthors(book.getAuthors());
+            original.setPublisher(book.getPublisher());
+            original.setFirstEdition(book.getFirstEdition());
+            original.setPublisher(book.getPublisher());
+            return true;
+        } else {
+            return false;
+        }
     }
 }
